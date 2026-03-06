46441926815e778f108cd752d7bc3a85a03973d8
Faisal-Aleesa <faisalaleesa55@gmail.com>
2026-03-06 23:23:39 +0300

    Completed Lab 4: HTML/CSS and Template Inheritance

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-path('', views.index, name='index'),
-path('<int:bookId>', views.viewbook) #add only this line of code
+    path('', views.index, name="books.index"),
+    path('list_books/', views.list_books, name="books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
 ]
