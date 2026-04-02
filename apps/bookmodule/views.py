c0eb0e387354a86dac4f7a579a6975b3f696a9aa
Faisal-Aleesa <faisalaleesa55@gmail.com>
2026-04-02 18:36:51 +0300

    Completed Lab 5: Links, Formatting, Lists, and Tables

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -11,3 +11,11 @@ def viewbook(request, bookId):
 
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
+def lab5_links(request):
+    return render(request, 'bookmodule/links.html')
+def lab5_formatting(request):
+    return render(request, 'bookmodule/formatting.html')
+def lab5_listing(request):
+    return render(request, 'bookmodule/listing.html')
+def lab5_tables(request):
+    return render(request, 'bookmodule/tables.html')
